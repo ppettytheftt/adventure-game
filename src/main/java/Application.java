0663927c0a9284a3b293a0a8088f10c2074cf7c4
ppettytheftt <jason.c.coffman23@gmail.com>
2021-10-19 20:16:03 -0500

    failed to add connection for postgresql database

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,14 @@
 import Gamelogic.GameLogic;
 
+import java.sql.SQLException;
+
 public class Application {
     public static void main(String[] args) {
-    GameLogic.startGame();
+        try {
+            JDBCAreaDAO.connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        GameLogic.startGame();
     }
 }
--- src/main/java/JDBCAreaDAO.java
+++ src/main/java/JDBCAreaDAO.java
@@ -3,8 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCAreaDAO {
-
-    public Connection connect() throws SQLException {
+    private final String url = "jdbc:postgresql://localhost:5432/";
+    private final String user = "postgres";
+    private final String password = "postGres1";
+    public static Connection connect() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
 }
